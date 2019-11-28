3d14bf7d77ae76957690c550dd8661b34866eb33
truongbb <truongbb96@outlook.com>
2019-11-28 23:19:46 +0700

    20191128_truongbb: config swagger

--- src/main/java/vn/ntqsolution/smart_shop/configuration/WebSecurityConfig.java
+++ src/main/java/vn/ntqsolution/smart_shop/configuration/WebSecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import vn.ntqsolution.smart_shop.security.JwtAuthenticationEntryPoint;
 import vn.ntqsolution.smart_shop.security.JwtRequestFilter;
 import vn.ntqsolution.smart_shop.utils.Constants;
@@ -51,7 +52,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     return super.authenticationManagerBean();
   }
 
-//  https://www.ninjadevcorner.com/2018/09/stateless-authentication-jwt-secure-spring-boot-rest-api.html
+  //  https://www.ninjadevcorner.com/2018/09/stateless-authentication-jwt-secure-spring-boot-rest-api.html
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
@@ -61,7 +62,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
       // don't create session
       .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
       .authorizeRequests()
-      .antMatchers(HttpMethod.GET, "/", "/*.html", "/favicon.ico", "/**/*.html", "/**/*.css", "/**/*.js").permitAll()
+      .antMatchers("/swagger-resources/**", "/v2/api-docs").permitAll()
+      .antMatchers(HttpMethod.GET, "/", "**/*.html", "/favicon.ico", "/**/*.html", "/**/*.css", "/**/*.js").permitAll()
       .antMatchers(HttpMethod.OPTIONS).permitAll()
       .antMatchers("/**/register", "/**/authenticate", "/**/active-account/**").permitAll()
 //      .antMatchers("/").hasRole(Constants.Role.CUSTOMER)
